78cc25d4f554ef48ab730868f5fa380577b9ca75
Keegan Nair <keegan.nair@hotmail.com>
2020-01-07 15:20:17 +1100

    Joined all 3 tables into one main table in sql

--- seed.sql
+++ seed.sql
@@ -23,12 +23,22 @@ PRIMARY KEY(id)
 );
 
 select * from role;  
--- role not working!!!!!! CSV file not showing in result grid
 
 CREATE TABLE department(
 id INT NOT NULL,
-name VARCHAR(30) NOT NULL,
+department_name VARCHAR(30) NOT NULL,
 PRIMARY KEY(id)
 );	
 
 select * from department;
+
+-- SELECT employee.id, employee.first_name, employee.last_name
+-- FROM employee INNER JOIN department ON (employee.id = department.id ) 
+
+
+SELECT E.id, E.first_name, E.last_name, 
+D.name, R.title, R.salary
+  FROM employee E
+  JOIN role R ON R.Id = E.role_id
+  JOIN department D ON D.id = R.department_id 
+ORDER BY E.id
